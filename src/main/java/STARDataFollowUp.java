4babefae5286587a583c83a171dd0f7abbd53168
Bharatblade <johnjp2015@gmail.com>
2025-03-29 19:40:16 -0400

    Initial commit

--- src/main/java/STARDataFollowUp.java
+++ src/main/java/STARDataFollowUp.java
@@ -1,15 +1,13 @@
 import java.util.ArrayList;
 
-import org.apache.avro.generic.GenericRecord;
-
 public class STARDataFollowUp {
 	public String HOSP_FOLLOWUP;
 	public String PXSTATDATE_FOLLOWUP;	
 	public ArrayList<Integer> daysSinceDischarge = new ArrayList<Integer>();
 	
-	public STARDataFollowUp(GenericRecord nextRecord) {
-		HOSP_FOLLOWUP = nextRecord.get("HOSP").toString();
-		PXSTATDATE_FOLLOWUP = nextRecord.get("PXSTATDATE").toString();
+	public STARDataFollowUp(String hosp, String pxstatdate) {
+		HOSP_FOLLOWUP = hosp;
+		PXSTATDATE_FOLLOWUP = pxstatdate;
 	}
 	
 }
--- src/main/java/STARDataPatient.java
+++ src/main/java/STARDataPatient.java
@@ -1,51 +1,52 @@
 import java.util.ArrayList;
 
-import org.apache.avro.generic.GenericRecord;
-
 public class STARDataPatient {
 	
 	public String AGE_DATA;
 	public ArrayList<String>  BMICALC_DATA = new ArrayList<String>();
 	public String DIAB_DATA;
 	public ArrayList<String> DISCHARGEDATE_DATA = new ArrayList<String>();
-	public String FUNCSTATTCR_DATA;
-	public String FUNCSTATTRF_DATA;
 	public String FUNCSTATTRR_DATA;
 	public String HCVSEROSTATUS_DATA;
-	public ArrayList<String> MELDPELDLABSCORE_DATA = new ArrayList<String>();
-	public String MALIG_DATA;
+	public ArrayList<String> MELD_DATA = new ArrayList<String>();
 	public String PTCODE_DATA;
 	public String TRRIDCODE_DATA;
 
 	public ArrayList<STARDataFollowUp> followUps = new ArrayList<STARDataFollowUp>();
-	public boolean [] readmissions = new boolean[4];
+	public boolean readmitted = false;
 	public double averageBMI;
 	public double averageMELD;
+	
+	public STARDataPatient(String age, 
+							String bmicalc, 
+							String diab, 
+							String dischargeDate, 
+							String funcstattrr, 
+							String hcv,
+							String meld,
+							String code,
+							String TRR) {
+		AGE_DATA = age;
+		if(bmicalc.length() > 0)
+			BMICALC_DATA.add(bmicalc);
 
-	public STARDataPatient(GenericRecord nextRecord) {
-		for(int i = 0; i < readmissions.length; i++) {
-			readmissions[i] = false;
-		}
-		AGE_DATA = nextRecord.get("AGE").toString();
-		if(nextRecord.get("BMICALC").toString().length() > 0)
-			BMICALC_DATA.add(nextRecord.get("BMICALC").toString());
-
-		DIAB_DATA = nextRecord.get("DIAB").toString();
-		DISCHARGEDATE_DATA.add(nextRecord.get("DISCHARGEDATE").toString());
+		DIAB_DATA = diab;
+		DISCHARGEDATE_DATA.add(dischargeDate);
 		
-		FUNCSTATTCR_DATA = nextRecord.get("FUNCSTATTCR").toString();
-		FUNCSTATTRF_DATA = nextRecord.get("FUNCSTATTRF").toString();
-		FUNCSTATTRR_DATA = nextRecord.get("FUNCSTATTRR").toString();
-		HCVSEROSTATUS_DATA = nextRecord.get("HCVSEROSTATUS").toString();
-		if(nextRecord.get("MELDPELDLABSCORE").toString().length() > 0)
-			MELDPELDLABSCORE_DATA.add(nextRecord.get("MELDPELDLABSCORE").toString());
-		MALIG_DATA = nextRecord.get("MALIG").toString();
-		PTCODE_DATA = nextRecord.get("PTCODE").toString();
-		TRRIDCODE_DATA = nextRecord.get("TRRIDCODE").toString();
+		FUNCSTATTRR_DATA = funcstattrr;
+		HCVSEROSTATUS_DATA = hcv;
+		
+		
+		if(meld.length() > 0)
+			MELD_DATA.add(meld);
+		PTCODE_DATA = code;
+		TRRIDCODE_DATA = TRR;
 	}
 	
-	public void addFollowUp(GenericRecord nextRecord) {
-		followUps.add(new STARDataFollowUp(nextRecord));
+	
+	public void addFollowUp(String hosp, String pxstatdate) {
+		followUps.add(new STARDataFollowUp(hosp, pxstatdate));
 	}
+
 	
 }
